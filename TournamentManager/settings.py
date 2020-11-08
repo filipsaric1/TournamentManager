32ff3e3f741214855a69cad017657c01491f99fd
unknown <fico.zd@hotmail.com>
2020-11-08 14:06:51 +0100

    env

--- TournamentManager/settings.py
+++ TournamentManager/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = '#&uu$r*df-fmal8k$2kpu+*#w%^4mcg)my(9^p2s2y&u*wl9^9'#os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
